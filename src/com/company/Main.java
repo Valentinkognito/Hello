20ac0df46b74926db896e6a6785751ce4e3ffad3
Valentinkognito <thevalik_mail@ua.fm>
2016-02-14 10:18:54 +0200

    izmenenie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         int b = 9;
         int a = 4;
+         if(  b < 10){
+            b = a;
+        }
+        System.out.println(b);
     }
+
 }
